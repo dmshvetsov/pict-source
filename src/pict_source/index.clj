ef40ad415f089575a8aa6717a48e60f4006211ac
shvetsovdm <sorrows@mail.ru>
2015-05-30 00:24:02 +1000

    Add index page info

--- src/pict_source/index.clj
+++ src/pict_source/index.clj
@@ -4,4 +4,19 @@
 (defn page []
   [:div {:class "content"} 
    [:div {:class "row"}
-    [:p "Открытый словарь программистов, от программистов, для повышения культуры использовния собственного языка в программировании."]]])
+    [:div {:class "six columns"}
+     [:p "Открытый словарь программистов, от программистов, для повышения культуры использовния собственного языка в программировании."]]
+    [:div {:class "six columns"}
+     [:p "Open programmers dictionary, by programmers, for the culture of using a national language in the programming environment."]]]
+   [:div {:class "row"}
+    [:div {:class "six columns"}
+     [:p "Сайт в данный момент работает в демонстрационном режиме."]]
+    [:div {:class "six columns"}
+     [:p "The site is working for demo purpose for now."]]]
+   [:div {:class "row"}
+    [:div {:class "six columns"}
+     [:p "Ищем людей для развития проекта, обращаться на "
+      [:a {:href "mailto:shvetsovdm@gmail.com"} "shvetsovdm@gmail.com"]]]
+    [:div {:class "six columns"}
+     [:p "Desperately looking for native Engilsh speaking contributer, email to "
+      [:a {:href "mailto:shvetsovdm@gmail.com"} "shvetsovdm@gmail.com"]]]]])
