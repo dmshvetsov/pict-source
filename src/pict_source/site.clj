914e1ea181db1d5d3b3a929c1d84861ede5495e7
shvetsovdm <sorrows@mail.ru>
2015-05-31 17:53:25 +1000

    Add google analytics

--- src/pict_source/site.clj
+++ src/pict_source/site.clj
@@ -4,6 +4,17 @@
 
 (def title "Pict")
 (def slogan "открытый словарь программистов")
+(def google-analytics-code
+  "<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-47078794-4', 'auto');
+    ga('send', 'pageview');
+
+  </script>")
 
 (defn layout [req content]
   (html5
@@ -28,4 +39,5 @@
        [:p {:class "copyright"}
         "&copy; 2015 "
         [:a {:href "http://shvetsovdm.github.io"}
-         "Дмитрий Швецов / Dmitry Shvetsov (shvetsovdm)"]]]]]))
+         "Дмитрий Швецов / Dmitry Shvetsov (shvetsovdm)"]]]]
+     google-analytics-code]))
