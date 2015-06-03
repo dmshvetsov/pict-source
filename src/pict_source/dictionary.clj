1e37182baa1ccb7e50a78ff9cf3994cd01c7231f
shvetsovdm <sorrows@mail.ru>
2015-06-03 23:31:53 +1000

    Add dinamicly build lang page

--- src/pict_source/dictionary.clj
+++ src/pict_source/dictionary.clj
@@ -1,12 +1,36 @@
 (ns pict-source.dictionary
   (:require [clojure.java.io :as io]
+            [clojure.string :as string]
             [cheshire.core :refer :all]))
 
 (def source-dir "resources/dictionary")
 (def dictionary-seq (file-seq (io/file source-dir)))
 
-(defn only-json [files-seq]
+(defn- only-json [files-seq]
   (filter #(and (.isFile %) (re-find #"\.json$" (.getName %))) files-seq))
 
-(defn read-word [word-file]
-  (parse-stream (io/reader word-file)))
+(defn- only-lang [lang files-seq]
+  (filter #(= lang (nth (reverse (string/split (.getName %) #"/")) 2)) files-seq))
+
+(defn- first-letter [word-file]
+  (first (.getName word-file)))
+
+(defn- read-word [word-file]
+  (parse-stream (io/reader word-file) true))
+
+(defn- parse-files [files-seq]
+  (map #(into
+          {:letter (string/capitalize (first-letter %))
+           :word (string/capitalize (string/replace (.getName %) #"\.json" ""))}
+          (read-word %)) files-seq))
+
+(defn- group-by-letter [words-seq]
+  (group-by :letter words-seq))
+
+(defn lang-map [lang]
+  (->> dictionary-seq
+      only-json
+      ;; FIXME
+      ;; only-lang lang
+      parse-files
+      group-by-letter))
--- src/pict_source/lang.clj
+++ src/pict_source/lang.clj
@@ -1,16 +1,17 @@
 (ns pict-source.lang
-  (:require [hiccup.page :refer [html5]]))
+  (:require [hiccup.page :refer [html5]]
+            [pict-source.dictionary :as dictionary]))
 
 (defn page []
-  [:div {:class "content"}
-   [:div {:class "row"}
-    [:div {:class "twelve columns pictionary--letter"} 
-     [:h2 "A"]]]
-   [:div {:class "row"}
-    [:div {:class "two columns"} "&nbsp;"]
-    [:div {:class "five columns pictionary--word"} "Абстракция данных"]
-    [:div {:class "five columns pictionary--word"} "Abstraction"]]
-   [:div {:class "row"}
-    [:div {:class "two columns pictionary--letter"} "&nbsp;"]
-    [:div {:class "five columns pictionary--word"} "Алгоритм"]
-    [:div {:class "five columns pictionary--word"} "Algorithm"]]])
+  (let [lang-map (dictionary/lang-map "ru")]
+    [:div {:class "content"}
+     (for [words-by-letter lang-map]
+       [:div
+        [:div {:class "row"}
+         [:div {:class "twelve columns pictionary--letter"}
+          [:h2 (first words-by-letter)]]]
+        (for [word-map (last words-by-letter)]
+          [:div {:class "row"}
+           [:div {:class "two columns pictionary--letter"} "&nbsp;"]
+           [:div {:class "five columns pictionary--word"} (:word word-map)]
+           [:div {:class "five columns pictionary--word"} (:translation word-map)]])])]))
