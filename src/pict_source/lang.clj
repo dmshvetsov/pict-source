c6bccc808b5de4ea9fc43ce2175cfadc607aa48d
shvetsovdm <sorrows@mail.ru>
2015-06-04 22:14:40 +1000

    Improve spacing on lang pages for small devices

--- src/pict_source/lang.clj
+++ src/pict_source/lang.clj
@@ -6,12 +6,12 @@
   (let [lang-map (dictionary/lang-map "ru")]
     [:div {:class "content"}
      (for [words-by-letter lang-map]
-       [:div
+       (list
         [:div {:class "row"}
          [:div {:class "twelve columns pictionary--letter"}
           [:h2 (first words-by-letter)]]]
         (for [word-map (last words-by-letter)]
           [:div {:class "row"}
-           [:div {:class "two columns pictionary--letter"} "&nbsp;"]
+           [:div {:class "two columns"} "&nbsp;"]
            [:div {:class "five columns pictionary--word"} (:word word-map)]
-           [:div {:class "five columns pictionary--word"} (:translation word-map)]])])]))
+           [:div {:class "five columns pictionary--word"} (:translation word-map)]])))]))
